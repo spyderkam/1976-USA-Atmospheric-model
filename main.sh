45c7615d0d3be93a9f41fbb8173732e1c20a34b3
Kam <kamyarmojtahed@gmail.com>
2025-08-22 01:18:29 +0000

    Update to the printing 60 `=`'s.

--- main.sh
+++ main.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 echo "Compiling and running the C++ source code..."
-
 # Compile the C++ source code
 g++ -o atmosphere src/cpp/main.cpp -lboost_math_c99
-
 # Run the compiled program and then delete the executable
 ./atmosphere
 rm -f atmosphere
 
-printf "%.0s=" {1..60}; printf "\n"
+printf '\n%60s\n\n' | tr ' ' '='
 
-echo "Now running the Python script..."
-
+printf "Now running the Python script..."
 python src/python/main.py
