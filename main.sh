2cd980c18c7fce5b36480aae3c6163d68afabee8
Kam <kamyarmojtahed@gmail.com>
2025-08-22 00:21:01 +0000

    Update to paths
    Updated paths to reflect deletion of `1976-USA-Atmosphere` directory.

--- main.sh
+++ main.sh
@@ -3,15 +3,14 @@
 echo "Compiling and running the C++ source code..."
 
 # Compile the C++ source code
-g++ -o atmosphere 1976-USA-Atmosphere/src/cpp/main.cpp -lboost_math_c99
+g++ -o atmosphere src/cpp/main.cpp -lboost_math_c99
 
-# Run the compiled program
+# Run the compiled program and then delete the executable
 ./atmosphere
-
 rm -f atmosphere
 
 printf "%.0s=" {1..60}; printf "\n"
 
 echo "Now running the Python script..."
 
-python 1976-USA-Atmosphere/src/python/main.py
+python src/python/main.py
